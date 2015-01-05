696f8625183afb3f66b04668eacac6bfe5473517
wildfield <wildfield@live.ru>
2015-01-05 22:19:03 +0200

    Initial commit

---
+++ src/peg/Elements.scala
@@ -0,0 +1,6 @@
+package peg
+
+object Elements extends Enumeration {
+  type Elements = Value
+  val Wall, Peg, Hole = Value
+}
---
+++ src/peg/Main.scala
@@ -0,0 +1,11 @@
+package peg
+
+import Elements._
+
+object Main {
+  val gameBoard = Array[Elements](Peg, Peg, Hole, Peg)
+  
+  def main(args: Array[String]) {
+      println("Hello, world!")
+   }
+}
